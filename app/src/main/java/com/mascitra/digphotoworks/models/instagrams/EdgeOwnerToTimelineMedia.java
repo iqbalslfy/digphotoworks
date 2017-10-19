01f9f982f92c2c4b299a1f6303c1dd2635ea0c61
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-10-19 14:06:52 +0700

    add getter setter

--- app/src/main/java/com/mascitra/digphotoworks/models/instagrams/EdgeOwnerToTimelineMedia.java
+++ app/src/main/java/com/mascitra/digphotoworks/models/instagrams/EdgeOwnerToTimelineMedia.java
@@ -20,4 +20,27 @@ public class EdgeOwnerToTimelineMedia {
     @Expose
     public List<Edge> edges = null;
 
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public PageInfo getPageInfo() {
+        return pageInfo;
+    }
+
+    public void setPageInfo(PageInfo pageInfo) {
+        this.pageInfo = pageInfo;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
 }
