53e4c06d11ddebc2cde4bef98cb21bc63ccae426
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-10-19 14:04:41 +0700

    update gallery activity

--- app/src/main/java/com/mascitra/digphotoworks/activity/Gallery.java
+++ app/src/main/java/com/mascitra/digphotoworks/activity/Gallery.java
@@ -6,18 +6,36 @@ import android.graphics.Bitmap;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
+import com.mascitra.digphotoworks.AppsCore;
 import com.mascitra.digphotoworks.R;
+import com.mascitra.digphotoworks.adapter.ImageAdapter;
+import com.mascitra.digphotoworks.models.instagrams.Edge;
+import com.mascitra.digphotoworks.networks.RetrofitApi;
+import com.mascitra.digphotoworks.responses.InstagramResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Gallery extends AppCompatActivity {
     private WebView webView = null;
     private ProgressDialog progressBar;
+    ImageAdapter imageAdapter;
+    GridView gridview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,35 +44,46 @@ public class Gallery extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        webView = (WebView) findViewById(R.id.webIG);
-        webView.loadUrl("https://webgram.co/digphotoworks");
+        imageAdapter = new ImageAdapter(this);
+
+        loadInsatgram();
+
+        gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(imageAdapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(Gallery.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+    }
 
-        webView.setWebViewClient(new WebViewClient() {
-
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                if (progressBar!= null && progressBar.isShowing()) {
-                    progressBar.dismiss();
+    public void loadInsatgram()
+    {
+        Call<InstagramResponse> call;
+        call = RetrofitApi.getInstance(true).getApiService("").instagram();
+        call.enqueue(new Callback<InstagramResponse>() {
+            @Override
+            public void onResponse(Call<InstagramResponse> call, Response<InstagramResponse> response) {
+                if(response.isSuccessful()) {
+                    List<Edge> edges = response.body().getData().getUser().getEdgeOwnerToTimelineMedia().getEdges();
+                    for (int i = 0; i < edges.size();i++){
+                        String thumbnail_src = edges.get(i).getNode().getThumbnailSrc();
+                        imageAdapter.addImage(thumbnail_src);
+                    }
+                    Log.d("isine",imageAdapter.getCount()+"");
+                    gridview.invalidateViews();
                 }
-                progressBar = ProgressDialog.show(Gallery.this, "DiG Photoworks", "Loading...");
             }
 
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-
-                return true;
+            @Override
+            public void onFailure(Call<InstagramResponse> call, Throwable t) {
+                Toast.makeText(Gallery.this, AppsCore.ERROR_NETWORK, Toast.LENGTH_SHORT).show();
             }
-
-            public void onPageFinished(WebView view, String url) {
-                if (progressBar.isShowing()) {
-                    progressBar.dismiss();
-                }
-            }
-
-
         });
     }
 
