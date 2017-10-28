04722ea75277154cdfebd0cff3b084ef3c980470
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-10-28 13:54:46 +0700

    update api service

--- app/src/main/java/com/mascitra/digphotoworks/networks/ApiService.java
+++ app/src/main/java/com/mascitra/digphotoworks/networks/ApiService.java
@@ -4,6 +4,7 @@ import com.mascitra.digphotoworks.responses.InstagramResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by blegoh on 19/10/17.
@@ -11,6 +12,7 @@ import retrofit2.http.GET;
 
 public interface ApiService {
 
-    @GET("?query_id=17888483320059182&variables={\"id\":\"29614503\",\"first\":12}")
-    Call<InstagramResponse> instagram();
+    @GET("?query_id=17888483320059182")
+    Call<InstagramResponse> instagram(@Query("variables") String variables);
+
 }
