cf9c2570bc8ca27591603c17cd2cbd4daf2343f7
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-10-31 15:33:45 +0700

    update transaksi activity

--- app/src/main/java/com/mascitra/digphotoworks/activities/Transaksi.java
+++ app/src/main/java/com/mascitra/digphotoworks/activities/Transaksi.java
@@ -20,8 +20,9 @@ import com.mascitra.digphotoworks.models.Product;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class Transaksi extends AppCompatActivity implements View.OnClickListener {
+public class Transaksi extends AppCompatActivity {
 
     @BindView(R.id.txtPaketDetail)
     TextView tvpaket;
@@ -36,7 +37,7 @@ public class Transaksi extends AppCompatActivity implements View.OnClickListener
     TextView tvdetail;
 
     @BindView(R.id.txt_jml_tmbh_pp)
-    TextView tvtambahan;
+    TextView tvTambahan;
 
     @BindView(R.id.txt_hasil_final_pp)
     TextView tvHasilFinal;
@@ -45,10 +46,10 @@ public class Transaksi extends AppCompatActivity implements View.OnClickListener
     TextView tvHasilFinal2;
 
     @BindView(R.id.imgPemesananDetail)
-    ImageView  img;
+    ImageView img;
 
     @BindView(R.id.jmlPlus)
-    TextView jmlText;
+    TextView jmlPlus;
 
     @BindView(R.id.btnMin)
     ImageButton btnMin;
@@ -59,9 +60,13 @@ public class Transaksi extends AppCompatActivity implements View.OnClickListener
     @BindView(R.id.btnSubmit)
     Button btnPesan;
 
-    Integer jmlAngka,v2,v3,Tambahan,hargaAwal,Total,Total2;
+    int jmlAngka;
+    int hargaTambah;
+    int tambahan = 0;
 
     String hasil;
+
+    Product product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,54 +76,32 @@ public class Transaksi extends AppCompatActivity implements View.OnClickListener
 
         Bundle bundle = getIntent().getExtras();
 
-        Product product = bundle.getParcelable("product");
+        product = bundle.getParcelable("product");
 
-        jmlText.setText("0");
+        jmlPlus.setText(tambahan+"");
 
         tvpaket.setText(product.getName());
-        tvharga.setText(product.getPrice()+"");
-        tvhargaAwal.setText(product.getPrice()+"");
-        tvHasilFinal.setText(product.getPrice()+"");
-        tvHasilFinal2.setText(product.getPrice()+"");
+        tvharga.setText(product.getPrice() + "");
+        tvhargaAwal.setText(product.getPrice() + "");
+        tvHasilFinal.setText(product.getPrice() + "");
+        tvHasilFinal2.setText(product.getPrice() + "");
+        hargaTambah = product.getPricePlus();
         tvdetail.setText(product.getDetail());
-        String url = AppsCore.BASE_IMAGE+product.getImage();
+        String url = AppsCore.BASE_IMAGE + product.getImage();
 
         GlideUrl glideUrl = new GlideUrl(url, new LazyHeaders.Builder()
                 .build());
         Glide.with(this).load(glideUrl).into(img);
 
-        btnMin.setOnClickListener(this);
-        btnPlus.setOnClickListener(this);
-        btnPesan.setOnClickListener(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
     }
 
-    void decimal(){
-        double hrg_awl = Double.parseDouble(tvharga.getText().toString());
-        double hrg_awl2 = Double.parseDouble(tvhargaAwal.getText().toString());
-        double hrg_tmbahan = Double.parseDouble(tvtambahan.getText().toString());
-        double total1 = Double.parseDouble(tvHasilFinal.getText().toString());
-
-
-        String hrg_awls = String.format("%,.0f", hrg_awl).replaceAll(",", ".");
-        String hrg_awls2 = String.format("%,.0f", hrg_awl2).replaceAll(",", ".");
-        String hrg_tmbahans = String.format("%,.0f", hrg_tmbahan).replaceAll(",", ".");
-        String total1s = String.format("%,.0f", total1).replaceAll(",", ".");
-
-
-        tvharga.setText(hrg_awls);
-        tvhargaAwal.setText(hrg_awls2);
-        tvtambahan.setText(hrg_tmbahans);
-        tvHasilFinal.setText(total1s);
-        tvHasilFinal2.setText(total1s);
-    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == android.R.id.home){
+        if (id == android.R.id.home) {
             onBackPressed();
             return true;
         }
@@ -126,96 +109,36 @@ public class Transaksi extends AppCompatActivity implements View.OnClickListener
 
     }
 
-    public void convert (){
-        jmlAngka = Integer.valueOf((jmlText.getText().toString()));
-        v2 = Integer.valueOf(("1"));
-        v3 = Integer.valueOf(("10000"));
-        Tambahan = Integer.valueOf((tvtambahan.getText().toString()));
-        hargaAwal = Integer.valueOf((tvhargaAwal.getText().toString()));
-        Total = Integer.valueOf((tvHasilFinal.getText().toString()));
-        Total2 = Integer.valueOf((tvHasilFinal2.getText().toString()));
+    @OnClick(R.id.btnPlus)
+    public void increase() {
+        tambahan++;
+        updateView();
     }
 
-    public void FuncAngkaUp(){
-        convert();
-        hasil = String.valueOf(jmlAngka+v2);
-        jmlText.setText(hasil);
-    }
-
-    public void FuncAngkaDown(){
-        convert();
-        hasil = String.valueOf(jmlAngka-v2);
-        jmlText.setText(hasil);
-    }
-
-    public void PerhitunganTambahan(){
-        convert();
-        hasil = String.valueOf(jmlAngka*v3);
-        System.out.println("penambahan : "+hasil);
-        tvtambahan.setText(hasil);
-    }
-
-    public void TotalFinal(){
-        convert();
-        hasil = String.valueOf(hargaAwal+Tambahan);
-        tvHasilFinal.setText(hasil);
-        tvHasilFinal2.setText(hasil);
-    }
-
-    public void PenguranganFinal(){
-        convert();
-        hasil = String.valueOf(Total-v3);
-        tvHasilFinal.setText(hasil);
-        tvHasilFinal2.setText(hasil);
-        if (Integer.parseInt(tvHasilFinal.getText().toString()) < Integer.parseInt(tvharga.getText().toString())){
-            Toast.makeText(this, "Jangan dilanjutkan, karena melewati  " + tvharga.getText(), Toast.LENGTH_LONG).show();
-            tvHasilFinal.setText(tvharga.getText());
-            tvHasilFinal2.setText(tvharga.getText());
-            jmlText.setText("0");
+    @OnClick(R.id.btnMin)
+    public void decrease() {
+        if (tambahan > 0) {
+            tambahan--;
+            updateView();
         }
     }
 
-    public void PenguranganTambahan(){
-        convert();
-        hasil = String.valueOf(jmlAngka*v3);
-        System.out.println("penambahan : "+hasil);
-        tvtambahan.setText(hasil);
-
+    public void updateView() {
+        jmlPlus.setText(tambahan+"");
+        tvTambahan.setText((hargaTambah * tambahan) + "");
+        tvHasilFinal.setText((product.getPrice() + (hargaTambah * tambahan)) + "");
+        tvHasilFinal2.setText((product.getPrice() + (hargaTambah * tambahan)) + "");
     }
 
-
-    @Override
-    public void onClick(View view) {
-        if (view == btnMin){
-            FuncAngkaDown();
-            PenguranganFinal();
-            PenguranganTambahan();
-
-
-        }
-
-        if (view == btnPlus){
-            FuncAngkaUp();
-            PerhitunganTambahan();
-            TotalFinal();
-
-        }
-
-        if (view == btnPesan){
-
-
-
-            Intent intent = new Intent(Transaksi.this, Pemesanan.class);
-
-            Bundle b = new Bundle();
-            b.putString("nm_paket", tvpaket.getText().toString());
-            b.putString("jml_tambahan", jmlText.getText().toString());
-            b.putString("hrg_awal", tvharga.getText().toString());
-            b.putString("tambahan", tvtambahan.getText().toString());
-            b.putString("total", tvHasilFinal.getText().toString());
-            intent.putExtras(b);
-            startActivity(intent);
-        }
-
+    @OnClick(R.id.btnSubmit)
+    public void submit() {
+        Intent i = new Intent();
+        Bundle b = new Bundle();
+        b.putParcelable("product", product);
+        b.putInt("tambahan",tambahan);
+        i.putExtras(b);
+        i.setClass(this, Pemesanan.class);
+        startActivity(i);
     }
+
 }
