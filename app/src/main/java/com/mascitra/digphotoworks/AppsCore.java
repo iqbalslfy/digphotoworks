fdaa083c9a6f7dbfc1600cf48eefcf781aa31574
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-02 15:56:13 +0700

    add new variable

--- app/src/main/java/com/mascitra/digphotoworks/AppsCore.java
+++ app/src/main/java/com/mascitra/digphotoworks/AppsCore.java
@@ -11,6 +11,8 @@ public class AppsCore extends Application{
     public static final String BASE_IMAGE = "http://188.166.179.34:9999/images/";
     public static final String INSTAGRAM = "https://www.instagram.com/graphql/query/";
     public static final String ERROR_NETWORK = "Error. Please check your internet connection";
+    public static final String YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/";
+    public static final String YOUTUBE_KEY = "AIzaSyDjw8VSvL3kB7HfcXSxb7KiPqr51O4OqIM";
     public static final String ERROR = "An error has occurred, ";
     public static final String TOKEN_EXPIRED = "Sesi login anda telah habis, silahkan melakukan login ulang !";
     public static final String TAG_TOKEN_EXPIRED = "token_expired";
