a72681e6110554aa1bb138a5971d290b94c0c5a9
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-06 15:22:42 +0700

    overide scroll

---
+++ app/src/main/java/com/mascitra/digphotoworks/scrolls/ScrollViewListener.java
@@ -0,0 +1,10 @@
+package com.mascitra.digphotoworks.scrolls;
+
+/**
+ * Created by blegoh on 06/11/17.
+ */
+
+public interface ScrollViewListener {
+    void onScrollChanged(ScrollViewExt scrollView,
+                         int x, int y, int oldx, int oldy);
+}
