22a4c8acf6afe18fbaf0153e03f928bfc7acae82
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-07 20:30:33 +0700

    update models

---
+++ app/src/main/java/com/mascitra/digphotoworks/models/Detail.java
@@ -0,0 +1,66 @@
+package com.mascitra.digphotoworks.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by blegoh on 07/11/17.
+ */
+
+public class Detail {
+    @SerializedName("id")
+    @Expose
+    public Integer id;
+    @SerializedName("name")
+    @Expose
+    public String name;
+    @SerializedName("promo_id")
+    @Expose
+    public Integer promoId;
+    @SerializedName("created_at")
+    @Expose
+    public String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    public String updatedAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPromoId() {
+        return promoId;
+    }
+
+    public void setPromoId(Integer promoId) {
+        this.promoId = promoId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
