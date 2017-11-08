0c1833d0103e78c505b4aee8cf1234ea3894fe99
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-08 10:21:21 +0700

    update detail promo

--- app/src/main/java/com/mascitra/digphotoworks/activities/DetailPromo.java
+++ app/src/main/java/com/mascitra/digphotoworks/activities/DetailPromo.java
@@ -2,6 +2,8 @@ package com.mascitra.digphotoworks.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,6 +11,9 @@ import android.widget.Toast;
 
 import com.mascitra.digphotoworks.AppsCore;
 import com.mascitra.digphotoworks.R;
+import com.mascitra.digphotoworks.adapters.DetailAdapter;
+import com.mascitra.digphotoworks.adapters.PromoAdapter;
+import com.mascitra.digphotoworks.models.Detail;
 import com.mascitra.digphotoworks.models.Product;
 import com.mascitra.digphotoworks.models.Promo;
 import com.mascitra.digphotoworks.networks.RetrofitApi;
@@ -16,6 +21,7 @@ import com.mascitra.digphotoworks.responses.BaseResponse;
 import com.mascitra.digphotoworks.responses.PromoResponse;
 import com.mascitra.digphotoworks.responses.PromoShowResponse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -43,6 +49,11 @@ public class DetailPromo extends AppCompatActivity {
     @BindView(R.id.tvPeriode)
     TextView tvPeriode;
 
+
+    private RecyclerView recyclerView;
+    private DetailAdapter detailAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +64,14 @@ public class DetailPromo extends AppCompatActivity {
         promo = bundle.getParcelable("promo");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         loadPromo(promo.getId());
+
+        recyclerView = (RecyclerView) findViewById(R.id.rc_detail);
+        recyclerView.setHasFixedSize(false);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        detailAdapter = new DetailAdapter(this, new ArrayList<Detail>(0));
+        recyclerView.setAdapter(detailAdapter);
     }
 
     public void loadPromo(int id) {
@@ -67,6 +86,7 @@ public class DetailPromo extends AppCompatActivity {
                     tvDetail.setText(promo.getProduct().getDetail());
                     tvPeriode.setText("Periode Promo "+promo.getStart()+" - "+promo.getEnd());
                     tvHarga.setText(promo.getProduct().getPrice()+"");
+                    detailAdapter.updateDetails(promo.getDetails());
                 }
             }
 
