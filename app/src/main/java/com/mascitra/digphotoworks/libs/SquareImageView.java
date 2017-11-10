10411daa52ea235b25a9729bfb8c4f40dbe22351
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-10 19:56:27 +0700

    add new libs

---
+++ app/src/main/java/com/mascitra/digphotoworks/libs/SquareImageView.java
@@ -0,0 +1,28 @@
+package com.mascitra.digphotoworks.libs;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by blegoh on 10/11/17.
+ */
+
+public class SquareImageView extends android.support.v7.widget.AppCompatImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth()); //Snap to width
+    }
+}
