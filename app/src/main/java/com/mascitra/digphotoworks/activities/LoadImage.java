684e11e7fcf3b961973008bab306c248e54522d8
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2017-11-12 09:40:53 +0700

    load from ig

--- app/src/main/java/com/mascitra/digphotoworks/activities/LoadImage.java
+++ app/src/main/java/com/mascitra/digphotoworks/activities/LoadImage.java
@@ -3,17 +3,44 @@ package com.mascitra.digphotoworks.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.model.LazyHeaders;
 import com.mascitra.digphotoworks.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LoadImage extends AppCompatActivity {
+
+    @BindView(R.id.imgLoad)
+    ImageView imgView;
+
+    @BindView(R.id.txtImage)
+    TextView txtCaption;
+
+    String image;
+    String caption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_image);
-
+        ButterKnife.bind(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Bundle bundle = getIntent().getExtras();
+
+        image = bundle.getString("image");
+        caption = bundle.getString("caption");
+
+        GlideUrl glideUrl = new GlideUrl(image, new LazyHeaders.Builder()
+                .build());
+        Glide.with(this).load(glideUrl).into(imgView);
+
+        txtCaption.setText(caption);
 
     }
 
