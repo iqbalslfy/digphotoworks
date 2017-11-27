60b25701c29f4a33b311850bd5c5f3f5344ba1c4
DESKTOP-NA8JGMG\SONY <muhammad.iqbal04998@gmail.com>
2017-11-27 16:27:25 +0700

    update maps

--- app/src/main/java/com/mascitra/digphotoworks/About.java
+++ app/src/main/java/com/mascitra/digphotoworks/About.java
@@ -1,5 +1,6 @@
 package com.mascitra.digphotoworks;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.annotation.Nullable;
@@ -13,14 +14,25 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class About extends Fragment {
+import com.mascitra.digphotoworks.activities.MapsActivity;
 
+public class About extends Fragment implements View.OnClickListener {
+    TextView maps;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_about,container,false);
         TextView textView = view.findViewById(R.id.konten);
+        maps = view.findViewById(R.id.textDiG);
+        maps.setOnClickListener(this);
         textView.setText("Saya Hardiyan Digwiyono (Jember). Saya & team akan mengambil sisi terbaik Anda dalam bingkai foto. Hubungi DiG: 081357772570 | 580C7A93");
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == maps){
+            startActivity(new Intent(getActivity(), MapsActivity.class));
+        }
     }
 }
